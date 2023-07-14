80b7f0b4ff0297e3ef98929afc03ddcd6b44b45e
Audionoob <danielmvazquez.audio@gmail.com>
2023-07-14 16:16:49 +0200

    test: saucedemo wrong password

--- cypress/e2e/saucedemo.cy.js
+++ cypress/e2e/saucedemo.cy.js
@@ -1,10 +1,21 @@
 /// <reference types="Cypress" />
 describe('Saucedemo Login Page', () => {
 
+  beforeEach(() => {
+    cy.visit('https://www.saucedemo.com')
+  })
+
   it('standard_user login', () => {
-    cy.visit('https://www.saucedemo.com', { timeout: 300000})
     cy.get('[data-test="username"]').type('standard_user')
     cy.get('[data-test="password"]').type('secret_sauce')
     cy.get('[data-test="login-button"]').click()
     })
+
+  it('wrong password login attempt', () => {
+  cy.get('[data-test="username"]').type('standard_user')
+    cy.get('[data-test="password"]').type('secret_sauc')
+    cy.get('[data-test="login-button"]').click()
+    cy.get('[data-test="error"]')
+    })
+
 })
