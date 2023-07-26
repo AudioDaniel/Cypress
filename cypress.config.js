b186a53cd8ab3bdbbf170db1d41449772867e9b4
Audionoob <danielmvazquez.audio@gmail.com>
2023-07-26 11:53:34 +0200

    feat:mocha reportert

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,31 @@
 /// <reference types="Cypress" />
 const { defineConfig } = require("cypress");
+const { beforeRunHook, afterRunHook } = require('cypress-mochawesome-reporter/lib');
 
 
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
     chromeWebSecurity: false,
     //experimentalModifyObstructiveThirdPartyCode	: true,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('before:run', async (details) => {
+        console.log('override before:run');
+        await beforeRunHook(details);
+      });
+
+      on('after:run', async () => {
+        console.log('override after:run');
+        await afterRunHook();
+      });
     },
   },
 });
