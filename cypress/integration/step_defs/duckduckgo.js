e6d89480dbd0bc7a7fa5f3cbc718d85fa1dc981f
Audionoob <danielmvazquez.audio@gmail.com>
2023-08-08 14:22:03 +0200

    feat: demoqa tests

--- cypress/integration/step_defs/duckduckgo.js
+++ cypress/integration/step_defs/duckduckgo.js
@@ -1,3 +1,4 @@
+/// <reference types="Cypress" />
 import { When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
 When("I visit duckduckgo.com", () => {
@@ -5,9 +6,28 @@ When("I visit duckduckgo.com", () => {
 });
 
 Then("I should see a search bar", () => {
-  cy.get("input").should(
+  cy.xpath(`//input[@id="searchbox_input"]`).should(
     "have.attr",
     "placeholder",
-    "Search the web without being tracked"
   );
 });
+
+When("I visit demoqa.com", () => {
+  cy.visit("https://demoqa.com/");
+});
+
+When("I click Elements", () =>{
+  cy.get(':nth-child(1) > :nth-child(1) > .card-up').click();
+})
+
+When("I click Check Box section", () =>{
+  cy.get(':nth-child(1) > .element-list > .menu-list > #item-1').click();
+})
+
+Then("I click the checkbox", () =>{
+  cy.get('label').click()
+})
+
+Then("A text should appear", () => {
+  cy.get('#result').contains("You have selected")
+})
